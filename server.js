563c2805bbfb9bec80fc3e70e5a0ed4165fb5105
Mohammad Shahjalal Khan <shahjalalkhanrabbi@gmail.com>
2023-06-13 06:20:21 +0600

    updated with create user a  with encrpt password and login user

--- server.js
+++ server.js
@@ -3,10 +3,17 @@ require('dotenv').config()
 const express = require('express')
 
 const app = express()
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+
+app.use(bodyParser.json())
 
 // CONNECTION EVENTS
 const uri = process.env.MONGODB_URI
-const mongoose = require('mongoose');
+
 
 mongoose.connect(uri, { useNewUrlParser: true })
     .then(() => console.log('Connected!'))
@@ -21,8 +28,20 @@ mongoose.connection.on('error', function(err) {
     console.log('Mongoose default connection error: ');
 });
 
-const bodyParser = require('body-parser')
-app.use(bodyParser.json())
+
+//?user Schema
+const userSchema = new mongoose.Schema({
+    fname: String,
+    lname: String,
+    email: String,
+    password: String,
+    age: Number
+}, {
+    timestamps: true
+})
+
+//?model
+const User = mongoose.model('User', userSchema)
 
 
 //?ApI to check connection
@@ -30,53 +49,108 @@ app.get('/', (req, res) => {
     res.json({ mesaage: 'Welcome to my app' })
 })
 
-let users = []
-let lastId = 0
 
 //?API to create a user
-app.post('/users', (req, res) => {
-    const user = req.body
-    user.id = ++lastId
-    users.push(user)
-    res.status(201).json({ mesaage: 'Welcome to our app' })
-})
-
-app.get('/users', (req, res) => {
-    res.json(users)
-})
-
-app.get('/users/:id', (req, res) => {
-    const id = req.params.id
-    const user = users.find((u) => u.id == id)
-    if (user) {
-        res.json(user)
-    } else {
-        res.status(404).json({ message: 'User not found' })
+app.post('/users', async(req, res) => {
+    try {
+        const salt = await bcrypt.genSalt(10)
+        const hash = await bcrypt.hash(req.body.password, salt)
+        const password = hash
+        const userObj = {
+            fname: req.body.fname,
+            lname: req.body.lname,
+            email: req.body.email,
+            age: req.body.age,
+            password: password
+        }
+        const user = new User(userObj)
+        await user.save()
+        res.status(201).json(user)
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({ message: 'Something is wrong' })
     }
 })
 
-app.put('/users/:id', (req, res) => {
-    const id = req.params.id
-    const body = req.body
-    const user = users.find((u) => u.id == id)
-    if (user) {
-        user.fname = body.fname
-        user.lname = body.lname
-        res.json(user)
-    } else {
-        res.status(404).json({ message: 'User not found' })
-
+app.post('/users/login', async(req, res) => {
+    try {
+        const { email, password } = req.body
+        const user = await User.findOne({ email: email })
+        if (!user) {
+            res.status(401).json({ message: 'User not found' })
+        } else {
+            const isValidPassword = await bcrypt.compare(password, user.password)
+            if (!isValidPassword) {
+                res.status(401).json({ message: 'Password is wrong' })
+            } else {
+                const token = jwt.sign({ email: user.email, id: user._id }, 'secret')
+                const userObj = user.toJSON()
+                userObj['accessToken'] = token
+                res.status(200).json(userObj)
+            }
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Something is wrong' })
     }
 })
 
-app.delete('/users/:id', (req, res) => {
-    const id = req.params.id
-    const userIndex = users.findIndex((u) => u.id == id)
-    if (userIndex) {
-        users.splice(userIndex, 1)
+app.get('/users', async(req, res) => {
+    try {
+        const users = await User.find({})
         res.json(users)
-    } else {
-        res.status(404).json({ message: 'User not found' })
+    } catch (error) {
+        res.status(500).json({ message: 'Something is wrong' })
+
+    }
+})
+
+app.get('/users/:id', async(req, res) => {
+    try {
+        const id = req.params.id
+        const user = await User.findById(id)
+        if (user) {
+            res.json(user)
+        } else {
+            res.status(404).json({ message: 'User not found' })
+        }
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ message: 'Something is wrong' })
+
+    }
+})
+
+app.put('/users/:id', async(req, res) => {
+    try {
+        const id = req.params.id
+        const body = req.body
+        const user = await User.findByIdAndUpdate(id, body, { new: true })
+        if (user) {
+
+            res.json(user)
+        } else {
+            res.status(404).json({ message: 'User not found' })
+
+        }
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ message: 'Something is wrong' })
+    }
+})
+
+app.delete('/users/:id', async(req, res) => {
+    try {
+        const id = req.params.id
+        const user = await User.findByIdAndDelete(id)
+        if (user) {
+            res.json(user)
+        } else {
+            res.status(404).json({ message: 'User not found' })
+        }
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ message: 'Something is wrong' })
     }
 })
 
